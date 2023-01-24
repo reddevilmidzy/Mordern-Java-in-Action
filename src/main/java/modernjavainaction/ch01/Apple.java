e4cbc6b3cfc6269c6f899ab193e709275b96590e
은지 <midzy0228@gmail.com>
2023-01-24 19:44:26 +0900

    feat: Apple 클래스 구현

---
+++ src/main/java/modernjavainaction/ch01/Apple.java
@@ -0,0 +1,12 @@
+package src.main.java.modernjavainaction.ch01;
+
+public class Apple {
+
+    private Integer wight;
+    private Color color;
+
+    public Apple(Integer wight, Color color) {
+        this.wight = wight;
+        this.color = color;
+    }
+}
---
+++ src/main/java/modernjavainaction/ch01/Color.java
@@ -0,0 +1,9 @@
+package src.main.java.modernjavainaction.ch01;
+
+public enum Color {
+    GREEN("Green"),
+    RED("Red");
+
+    Color(String color) {
+    }
+}
